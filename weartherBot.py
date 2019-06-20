50798a7608d7c13c395ff8371cc4b10ebb6524fe
sneaky-moon <blue_dragon_guardian@hotmail.com>
2019-06-20 16:32:04 +1000

    somebody

--- weartherBot.py
+++ weartherBot.py
@@ -2,7 +2,7 @@ import keys
 import requests
 import tweepy
 
-url = ("https://api.darksky.net/forecast/""+keys.dark_sky+""/-37.8136,144.9631?units=si")
+url = ("https://api.darksky.net/forecast/{}/-37.8136,144.9631?units=si".format(keys.dark_sky))
 data = requests.get(url).json()
 
 #depending on the temperature and if it's gonna rain, print that message
@@ -18,8 +18,10 @@ if(maxTemp) < 15 and (riskOfRain) > 0.5:
 if(maxTemp) < 15 and (riskOfRain) < 0.5:
     message = "Good morning! It's gonna be a top of " + str((round(maxTemp))) + "°C today with a " + str((format(riskOfRain, ".0%"))) + " of rain, so maybe chuck on a coat before you leave the house!"
 
+
 #to expand on the printing of messages, should do like a randomiser bot Ai thing?
 #setting up tweepy and twitter bot
+
 account = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
 account.set_access_token(keys.access_token, keys.access_secret)
 bot = tweepy.API(account)
