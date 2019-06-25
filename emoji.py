cc2ae8538b2dfeef65106dcfe986e1e3546e7ac4
sneaky-moon <blue_dragon_guardian@hotmail.com>
2019-06-25 23:07:20 +1000

    oops try again

--- emoji.py
+++ emoji.py
@@ -1,4 +1,9 @@
 import emoji
 
-print("\N{grinning face}")
-  
+account = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+account.set_access_token(keys.access_token, keys.access_secret)
+bot = tweepy.API(account)
+
+message = "\N{grinning face}"
+
+bot.update_status(message)
