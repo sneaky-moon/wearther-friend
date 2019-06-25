14066cb859e0e0c1cf3647d657c42c3d252b0713
sneaky-moon <blue_dragon_guardian@hotmail.com>
2019-06-25 22:58:44 +1000

    ok this should be final for now

--- weartherBot.py
+++ weartherBot.py
@@ -1,6 +1,8 @@
 import keys
 import requests
 import tweepy
+import tweetGenerate
+import random
 
 url = ("https://api.darksky.net/forecast/{}/-37.8136,144.9631?units=si".format(keys.dark_sky))
 data = requests.get(url).json()
@@ -9,17 +11,23 @@ data = requests.get(url).json()
 
 maxTemp = data['daily']['data'][0]['temperatureHigh']
 riskOfRain = data['daily']['data'][0]['precipProbability']
+summary = data['daily']['data'][0]['summary']
 
-#if maxTemp is lower than 15 degrees and more than 50% chance of rain
-if(maxTemp) < 15 and (riskOfRain) > 0.5:
-    message = "Brrrr! It's a cold one today at a top of " + str((round(maxTemp))) + "°C with a " + str((format(riskOfRain, ".0%"))) + " of rain, so dress warmly and remember your umbrella!"
+if (maxTemp) < 15 and (riskOfRain) > 0.5:
+    message = random.choice(tweetGenerate.coldsfx) + ' ' + random.choice(tweetGenerate.coldword) + ' with a top of ' + str((round(maxTemp))) + '°C and a ' + str((format(riskOfRain, ".0%"))) + ' of rain, so ' + random.choice(tweetGenerate.dresswarm) + ' and remember your umbrella!'
 
-#if maxTemp is lower than 15 degrees and less than 50% chance of rain
-if(maxTemp) < 15 and (riskOfRain) < 0.5:
-    message = "Good morning! It's gonna be a top of " + str((round(maxTemp))) + "°C today with a " + str((format(riskOfRain, ".0%"))) + " of rain, so maybe chuck on a coat before you leave the house!"
+if (maxTemp) < 15 and (riskOfRain) < 0.5:
+    message = random.choice(tweetGenerate.coldsfx) + ' ' + random.choice(tweetGenerate.coldword) + ' with a top of ' + str((round(maxTemp))) + '°C and a ' + str((format(riskOfRain, ".0%"))) + ' of rain, so ' + random.choice(tweetGenerate.dresswarm) + ' before you leave the house!'
 
+if 15 < (maxTemp) < 25 and (riskOfRain) < 0.5:
+    message = random.choice(tweetGenerate.neutralsfx) + ' ' + random.choice(tweetGenerate.todayweather) + str(summary) + ' A top of ' + str((round(maxTemp))) + '°C and a ' + str((format(riskOfRain, ".0%"))) + ' of rain, so ' + random.choice(tweetGenerate.dressneutral) + ' and ' + random.choice(tweetGenerate.neutralgreeting)
 
-#to expand on the printing of messages, should do like a randomiser bot Ai thing?
+if 15 < (maxTemp) < 25 and (riskOfRain) > 0.5:
+    message = random.choice(tweetGenerate.neutralsfx) + ' ' + random.choice(tweetGenerate.todayweather) + str(summary) + ' A top of ' + str((round(maxTemp))) + '°C and a ' + str((format(riskOfRain, ".0%"))) + ' of rain, so ' + random.choice(tweetGenerate.dressneutral) + ' and ' + ' and remember your umbrella!'
+
+if (maxTemp) > 25:
+    message = random.choice(tweetGenerate.hotsfx) + ' ' + random.choice(tweetGenerate.todayweather) + str(summary) + ' A top of ' + str((round(maxTemp))) + '°C and a ' + str((format(riskOfRain, ".0%"))) + ' of rain, so ' + random.choice(tweetGenerate.dresslight) + ' and ' + 'protect yourself from the sun!'
+
 #setting up tweepy and twitter bot
 
 account = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
