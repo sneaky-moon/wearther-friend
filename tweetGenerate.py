a179effae2043bcb94ab8224e0bdc94c9f233c4c
sneaky-moon <blue_dragon_guardian@hotmail.com>
2019-06-25 23:37:16 +1000

    ok emoji should be working

--- tweetGenerate.py
+++ tweetGenerate.py
@@ -8,4 +8,4 @@ dressneutral= ['dress nice', 'dress clean', 'dress classy', 'dress comfortably',
 dresslight= ['dress lightly', 'dress cooly']
 neutralgreeting= ['have an awesome day!', 'have a nice day!', 'have a great day!', 'make the most of today!', 'enjoy your day']
 todayweather= ['Today\'s weather: ', 'The weather today: ', 'Melbourne today: ']
-emoji= ['\N{grinning face}', '\N{slightly smiling face}', '\N{grinning face with big eyes}', '\N{beaming face with smiling eyes}', '\N{smiling face}', '\N{grinning face with smiling eyes}', '\N{winking face}', '\N{smiling face with smiling eyes}', '\N{kissing face with closed eyes}']
+emoji= ['\U0001f603', '\U0001f604', '\U0001f600', '\U0001f601', '\U0001f606', '\U0001f642', '\U0001f643', '\U0001f609', '\U0001f60A', '\U0001263A', '\U0001f61A' ]
