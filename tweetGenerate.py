9105e03d6e3e89565828d30e637c5562dec717c1
sneaky-moon <blue_dragon_guardian@hotmail.com>
2019-08-21 12:55:46 +1000

    fixed a broken emoji

--- tweetGenerate.py
+++ tweetGenerate.py
@@ -8,4 +8,4 @@ dressneutral= ['dress nice', 'dress clean', 'dress classy', 'dress comfortably',
 dresslight= ['dress lightly', 'dress cooly']
 neutralgreeting= ['have an awesome day!', 'have a nice day!', 'have a great day!', 'make the most of today!', 'enjoy your day']
 todayweather= ['Today\'s weather: ', 'The weather today: ', 'Melbourne today: ']
-emoji= ['\U0001f603', '\U0001f604', '\U0001f600', '\U0001f601', '\U0001f606', '\U0001f642', '\U0001f643', '\U0001f609', '\U0001f60A', '\U0001263A', '\U0001f61A' ]
+emoji= ['\U0001f603', '\U0001f604', '\U0001f600', '\U0001f601', '\U0001f606', '\U0001f642', '\U0001f643', '\U0001f609', '\U0001f60A', '\U0001f61A' ]
